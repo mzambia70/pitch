8d65e04b1a225e9f8e146a8853f7f5538b47cc98
mzambia70 <mzambia70@gmail.com>
2019-05-29 11:01:00 +0300

    added a functionality for the snding of emails to the user

---
+++ app/email.py
@@ -0,0 +1,12 @@
+from flask_mail import Message
+from flask import render_template
+from . import mail
+
+
+def mail_message(subject,template,to,**kwargs):  #taking four parameters
+    sender_email = "mzambia70@gmail.com"
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
+    email.txt = render_template(template + ".txt",**kwargs)
+    mail.send(email)
